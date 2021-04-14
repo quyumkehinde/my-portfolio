b932b5fc4c484060006f4f03a5012f3521728e9a
Quyum Kehinde <Quyumkehinde@gmail.com>
2021-04-14 08:27:24 -0700

    Added the contact section

---
+++ app.js
@@ -0,0 +1,19 @@
+const toggle = () => {
+            var menuNode = document.querySelector(".mobile-menu");
+            if(menuNode.style.display == "none"){
+                menuNode.style.display = "block";
+                document.querySelector(".mobile-toggle:hover div:nth-child(1)").style.transform = "rotate(-45deg)";
+                document.querySelector(".mobile-toggle:hover div:nth-child(3)").style.transform = "rotate(45deg)";
+                document.querySelector(".mobile-toggle:hover div:nth-child(3)").style.margin = "-1px 0 0 0";
+                document.querySelector(".mobile-toggle:hover div:nth-child(1)").style.margin = "0 0 -1px 0";
+                document.querySelector(".mobile-toggle:hover div:nth-child(2)").style.display = "none";
+            } else if (menuNode.style.display == "block") {
+                document.querySelector(".mobile-toggle:hover div:nth-child(1)").style.transform = "rotate(0)";
+                document.querySelector(".mobile-toggle:hover div:nth-child(3)").style.transform = "rotate(0)";
+                document.querySelector(".mobile-toggle:hover div:nth-child(3)").style.margin = "3px 0";
+                document.querySelector(".mobile-toggle:hover div:nth-child(1)").style.margin = "3px 0";
+                document.querySelector(".mobile-toggle:hover div:nth-child(2)").style.display = "block";
+                menuNode.style.display = "none";
+            }
+            
+        }
